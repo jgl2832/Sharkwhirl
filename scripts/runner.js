d9c53b239a9b2a0ea7b1b0db0bca0687d2901ac4
Jake Levine <jgl2832@gmail.com>
2016-08-30 22:57:20 -0400

    new image for bassdude, bigger bassdude

--- scripts/runner.js
+++ scripts/runner.js
@@ -419,7 +419,7 @@ Q.Sprite.extend("BassDude", {
     this._super({
       x: player.p.x + Q.width + 50,
       y: 565,
-      scale: 2.0,
+      scale: 2.5,
       type: SPRITE_BOX,
       sheet: "bassdude",
       sprite: "bassdude",
