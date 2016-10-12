20519da1846fab393819791a050abb13f491dbc3
Jake Levine <jgl2832@gmail.com>
2016-10-11 23:56:28 -0400

    fix

--- scripts/runner.js
+++ scripts/runner.js
@@ -101,7 +101,7 @@ Q.Sprite.extend("Player",{
     } else if (isPlatform(coll.obj)) {
       this.p.landed = 1;
     } else {
-      if (!Q.state.get("invincible") {
+      if (!Q.state.get("invincible")) {
         this.die(coll);
       }
     }
